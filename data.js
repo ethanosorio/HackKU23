f22727eeca1e723b0bab6b0ed2870322976dec27
Ethan Osorio <ethan.osorio79@gmail.com>
2023-04-15 20:24:55 -0500

    moved images

--- data.js
+++ data.js
@@ -1,12 +1,13 @@
 import { supabase } from "./config/supabaseClient";
 
 let currentDate = new Date().toJSON().slice(0, 10);
+console.log(currentDate)
 
 const useData = async () => {
   const { data, error } = await supabase
     .from("Words")
-    .select()
-    .eq('day', currentDate);
-  console.log(data);
+    .select();
+    // .eq('day', currentDate);
+  console.log("data: ",data);
 };
 useData()
